780bc0d0432bf3e2442ad2dd6cb15400cb5c0d91
Raghu Kanchiraju <rgh.199@gmail.com>
2016-09-20 22:28:54 +0530

    Chapter 1 finished

--- rango/urls.py
+++ rango/urls.py
@@ -3,4 +3,5 @@ from rango import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='rango_index'),
+	url(r'about/$', views.about, name='rango_about'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-	return HttpResponse('Hey there! How are you?')
+	return HttpResponse('Hey there! How are you? <a href="/rango/about/">About</a>')
+
+def about(request):
+	return HttpResponse('Rango says here is the about page. <a href="/rango/">Home</a>')
