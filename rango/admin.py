b24c9c01b6fb09022a4032ea24a6c766192be87c
Raghu Kanchiraju <rgh.199@gmail.com>
2016-09-21 23:02:50 +0530

    Rango Admin site

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageInline(admin.TabularInline):
+	model = Page
+	extra = 3
+
+class CategoryAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Name', {'fields': ['name']}),
+		('Views & Likes', {'fields': ['views','likes'], 'classes': ('extrapretty',)})
+	]
+	inlines = [PageInline]
+	list_display = ('name','views','likes')
+	search_fields = ['name']
+
+class PageAdmin(admin.ModelAdmin):
+	# fields = ['title','category','url']
+	fieldsets = [
+		(None, {'fields': ['category']}),
+		('Page Title & URL', {'fields': ['title','url']}),
+	]
+	list_display = ('title','category','url')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
