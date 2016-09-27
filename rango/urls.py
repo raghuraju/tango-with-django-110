1053a1d4ed2f03d2f8debd0c83316a57a45df1a3
Raghu Kanchiraju <rgh.199@gmail.com>
2016-09-27 20:28:55 +0530

    Using url namespacing with app_name

--- rango/urls.py
+++ rango/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from rango import views
 
+app_name = 'rango'
 urlpatterns = [
-	url(r'^$', views.index, name='rango_index'),
-	url(r'about/$', views.about, name='rango_about'),
-	url(r'add-category/$', views.add_category, name='rango_add_category'),
-	url(r'add-page/(?P<category_slug_url>[\w\-]+)/$', views.add_page, name='rango_add_page'),
-	url(r'category/(?P<category_name_url>[\w\-]+)/$', views.show_category, name='rango_show_category'),
+	url(r'^$', views.index, name='index'),
+	url(r'about/$', views.about, name='about'),
+	url(r'add-category/$', views.add_category, name='add_category'),
+	url(r'add-page/(?P<category_slug_url>[\w\-]+)/$', views.add_page, name='add_page'),
+	url(r'category/(?P<category_name_url>[\w\-]+)/$', views.show_category, name='show_category'),
 ]
