5cc571188a802ffc4c3c3323782f3df7f9e9f77d
Raghu K <rgh.199@gmail.com>
2016-10-13 21:02:15 +0530

    typo in base.py

--- tango_with_django/settings/base.py
+++ tango_with_django/settings/base.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': DB_NAME,
         'USER': DB_USER,
-        'PASSWORD': DB_PASSOWRD,
+        'PASSWORD': DB_PASSWORD,
         'HOST': DB_HOST,
         'PORT': DB_PORT
     }
